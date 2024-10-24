1e50f9887776d4c39bbaa370272874e39ca43732
Рекс <r.tambua@walnut.team>
2024-10-24 11:56:02 +0300

    added base components

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./sections/Header.jsx";
+import Hero from "./sections/Hero.jsx";
+
+
+const  App = () => {
+  return (
+    <main className="overflow-hidden">
+      <Header />
+      <Hero />
+    </main>
+  );
+}
+
+export default App;
